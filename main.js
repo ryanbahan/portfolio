3fe2c093b1bdda3133a43f726f3d327a8bf2b521
Ryan Bahan <ryan@ryanbahan.com>
2020-04-04 16:27:00 -0600

    Begin structuring

--- main.js
+++ main.js
@@ -1,1 +1,3 @@
-console.log('hello');
+gsap.from('.partial-bg', {opacity: 0, xPercent: -125, duration: 1.5, ease: 'Power2.easeInOut'})
+
+gsap.from('nav', {yPercent: -150, duration: 2, ease: "expo.out"})
