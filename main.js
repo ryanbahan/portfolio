363917929fb3e159095e374c394bbfbca27c98f6
Ryan Bahan <ryan@ryanbahan.com>
2020-04-04 17:00:28 -0600

    Adjust padding

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 gsap.from('.partial-bg', {opacity: 0, xPercent: -125, duration: 1.25, ease: 'Power2.easeInOut'});
 
-gsap.from('nav', {yPercent: -150, duration: 2, ease: "expo.out", delay: 3});
+gsap.from('nav', {yPercent: -150, duration: 2, ease: "expo.out", delay: 3.5});
 
 gsap.from('.firstname', {opacity: 0, duration: 2.5, delay: 1.125});
 
@@ -8,6 +8,6 @@ gsap.from('.lastname', {opacity: 0, delay: 1.45, duration: 2});
 
 gsap.from('.skills-1', {opacity: 0, delay: 2.25});
 
-gsap.from('.skills-2', {opacity: 0, delay: 2.5});
+gsap.from('.skills-2', {opacity: 0, delay: 2.65});
 
-gsap.from('.skills-3', {opacity: 0, delay: 2.75});
+gsap.from('.skills-3', {opacity: 0, delay: 3.05});
