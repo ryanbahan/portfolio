fa1bca61d54b8fbb6b5caa5408711d06e45c3dc7
Ryan Bahan <ryan@ryanbahan.com>
2020-04-04 18:08:57 -0600

    Switch animations to timeline

--- main.js
+++ main.js
@@ -1,19 +1,12 @@
-gsap.from('.partial-bg', {opacity: 0, xPercent: -125, duration: 1.25, ease: 'Power2.easeInOut'});
+const timeline = gsap.timeline({defaults:{duration: 1}});
 
-gsap.from('nav', {y: -150, duration: 2, ease: "expo.out", delay: 3.5});
-
-gsap.from('.firstname', {y: -20, opacity: 0, duration: 0.75, delay: 1.125});
-
-gsap.from('.lastname', {y: -20, opacity: 0, delay: 1.45, duration: 0.75});
-
-gsap.from('.skills-1', {opacity: 0, delay: 2.25});
-
-gsap.from('.skills-2', {opacity: 0, delay: 2.65});
-
-gsap.from('.skills-3', {opacity: 0, delay: 3.05});
-
-gsap.from('.fa-github', {opacity: 0, delay: 4.5});
-
-gsap.from('.fa-linkedin-in', {opacity: 0, delay: 4.75});
-
-gsap.from('.fa-envelope-square', {opacity: 0, delay: 5});
+timeline.from(".partial-bg", {opacity: 0, xPercent: -125, duration: 1.25, ease: 'Power2.easeInOut'})
+.from('.firstname', {y: -20, opacity: 0, duration: 0.75}, "-=0.15")
+.from('.lastname', {y: -20, opacity: 0, duration: 0.75}, "-=0.45")
+.from('.skills-1', {opacity: 0}, "-=0.15")
+.from('.skills-2', {opacity: 0}, "-=0.5")
+.from('.skills-3', {opacity: 0}, "-=0.5")
+.from('nav', {y: -150, duration: 2, ease: "expo.out"}, "-=0.65")
+.from('.fa-github', {opacity: 0}, "-=0.75")
+.from('.fa-linkedin-in', {opacity: 0}, "-=0.75")
+.from('.fa-envelope-square', {opacity: 0}, "-=0.75")
